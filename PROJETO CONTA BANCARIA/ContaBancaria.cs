6d1d5679eb34d30e0fd0b8c0f32e3ef6a252e4c4
lucalmr <lucalmr12@gmail.com>
2023-06-26 20:43:11 -0300

    Adicionar arquivos de projeto.

---
+++ PROJETO CONTA BANCARIA/ContaBancaria.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJETO_CONTA_BANCARIA {
+    internal class ContaBancaria {
+        public int Numero { get; private set; }
+        public string Titular { get; private set; }
+
+        public double Saldo { get; private set; }
+
+        //1º Contrutor
+        public ContaBancaria(int numero, string titular) {
+            Numero = numero;
+            Titular = titular;
+        }
+        //2º Construtor
+        public ContaBancaria (int numero,string titular,double depositoinicial) : this (numero, titular) {
+
+            Deposito(depositoinicial);
+        }
+        //3º Metodos (calculos)
+        public void Deposito (double quantia) {
+            Saldo += quantia;
+        }
+        public void Saque (double quantia) {
+            //irei descontar sempre a quantia certa
+            // mais 5 reais de taxa para cada saque.
+            Saldo -= quantia + 5.00 ;
+        }
+
+        //tostring com override concat
+        public override string ToString() {
+            return "Conta: " + Numero
+                + ", Titular: " 
+                + Titular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ PROJETO CONTA BANCARIA/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJETO_CONTA_BANCARIA {
+    internal class Program {
+
+
+        static void Main(string[] args) {
+            ContaBancaria conta;
+            Console.Write("Entre com  o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Enre com titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Havera deposito inicial (S/N) ");
+            char resp = char.Parse(Console.ReadLine());
+
+            if (resp == 's' || resp == 'S') {
+                //caso tenha saldo inicial, iremos executar essa condição
+                Console.Write("Entre com deposito INICIAL: ");
+                double DepositoInicial = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                conta = new ContaBancaria(numero,titular,DepositoInicial);
+
+            }
+            //caso não tenha, irei utilizar o construtor com apenas 2 argumentos (numero,titular)
+            else {
+                conta = new ContaBancaria(numero, titular);
+                //ira entrar um saldo zerado ( devido int, ja iniciar com zero)
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da Conta: ");
+            Console.WriteLine(conta);
+            Console.WriteLine("------------------------\n----------------------");
+            Console.Write("Entre um valor para DEPOSITO: ");
+            double quantia = double.Parse(Console.ReadLine() ,CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.Write("Dados da conta Atualizados: ");
+            Console.WriteLine(conta);
+            //abaixo entrar com quantia de saque
+            Console.WriteLine("------------------------\n----------------------");
+            Console.Write("Entre um valor para SAQUE: ");
+             quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.Write("Dados da conta Atualizados: ");
+            Console.WriteLine(conta);
+            Console.ReadKey();
+        }
+    }
+}
